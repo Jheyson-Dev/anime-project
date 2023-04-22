055ff6ce8c0aa72624785fd84e1f7d9ea8f03e4e
Jheyson-Dev <Jheyson-Dev@users.noreply.github.com>
2023-04-22 16:52:01 -0500

    Creacion del componente Header con sus estilos

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,35 @@
+import { Link, NavLink } from 'react-router-dom';
+import logo from '../../assets/Logo.svg';
+import './Header.css';
+
+export const Header = () => {
+  return (
+    <header>
+      <div className='contenedor'>
+        <div>
+          <Link to={'/'}>
+            <img
+              src={logo}
+              alt='Logo Anime'
+              className='logo'
+            />
+          </Link>
+        </div>
+        <nav>
+          <ul className='flex gap-5'>
+            <li>
+              <div>
+                <NavLink
+                  to={'/'}
+                  className={isActive => (isActive ? 'active' : 'desactive')}
+                >
+                  Anime
+                </NavLink>
+              </div>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
